93a40644550b21316f5ab090337fb8cb1f7330bc
Vicky Hariadi <hariadivicky@gmail.com>
2020-04-07 00:20:19 +0700

    add router group

--- nano.go
+++ nano.go
@@ -6,10 +6,19 @@ package nano
 
 import "net/http"
 
+// RouterGroup defines collection of route that has same prefix
+type RouterGroup struct {
+	prefix string
+	engine *Engine
+	parent *RouterGroup
+}
+
 // Engine defines nano web engine.
 type Engine struct {
+	*RouterGroup
 	router *router
 	debug  bool
+	groups []*RouterGroup
 }
 
 // H defines json wrapper.
@@ -20,30 +29,56 @@ type HandlerFunc func(c *Context)
 
 // New is nano constructor
 func New() *Engine {
-	return &Engine{
+	engine := &Engine{
 		router: newRouter(),
 		debug:  false,
 	}
+
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+
+	return engine
+}
+
+// Group functions to create new router group.
+func (rg *RouterGroup) Group(prefix string) *RouterGroup {
+	group := &RouterGroup{
+		prefix: rg.prefix + prefix,
+		parent: rg,
+		engine: rg.engine,
+	}
+
+	rg.engine.groups = append(rg.engine.groups, group)
+
+	return group
 }
 
 // GET functions to register route with GET request method.
-func (ng *Engine) GET(urlPattern string, handler HandlerFunc) {
-	ng.router.addRoute(http.MethodGet, urlPattern, handler)
+func (rg *RouterGroup) GET(urlPattern string, handler HandlerFunc) {
+	rg.addRoute(http.MethodGet, urlPattern, handler)
 }
 
 // POST functions to register route with POST request method.
-func (ng *Engine) POST(urlPattern string, handler HandlerFunc) {
-	ng.router.addRoute(http.MethodPost, urlPattern, handler)
+func (rg *RouterGroup) POST(urlPattern string, handler HandlerFunc) {
+	rg.addRoute(http.MethodPost, urlPattern, handler)
 }
 
 // PUT functions to register route with PUT request method.
-func (ng *Engine) PUT(urlPattern string, handler HandlerFunc) {
-	ng.router.addRoute(http.MethodPut, urlPattern, handler)
+func (rg *RouterGroup) PUT(urlPattern string, handler HandlerFunc) {
+	rg.addRoute(http.MethodPut, urlPattern, handler)
 }
 
 // DELETE functions to register route with DELETE request method.
-func (ng *Engine) DELETE(urlPattern string, handler HandlerFunc) {
-	ng.router.addRoute(http.MethodDelete, urlPattern, handler)
+func (rg *RouterGroup) DELETE(urlPattern string, handler HandlerFunc) {
+	rg.addRoute(http.MethodDelete, urlPattern, handler)
+}
+
+// addRoute functions to register new route with current group prefix.
+func (rg *RouterGroup) addRoute(requestMethod, urlPattern string, handler HandlerFunc) {
+	// append router group prefix.
+	prefixedURLPattern := rg.prefix + urlPattern
+
+	rg.engine.router.addRoute(requestMethod, prefixedURLPattern, handler)
 }
 
 // ServeHTTP implements multiplexer.
